84be5f7d990f847a6b2ae0810e02eb9c7d928e0a
Mitchel <mitchel_jager@hotmail.com>
2019-08-22 20:14:25 +0200

    Added bonsai url env to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV["BONSAI_URL"]
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
