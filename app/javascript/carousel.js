8a8f8e085f2931995163ebe83bde110e3584c4a3
Mitchel <mitchel_jager@hotmail.com>
2019-09-11 00:56:33 +0200

    Added lazyload to carousel

--- app/javascript/carousel.js
+++ app/javascript/carousel.js
@@ -31,4 +31,27 @@ function setActiveItem() {
 
   if (activeElement) activeElement.classList.remove("carousel__item--is-active")
   if (navigationElements.length) navigationElements[this.currentSlide].classList.add("carousel__item--is-active")
+
+  setLazyImage(this)
+}
+
+function setLazyImage(element) {
+  const slides = []
+  slides.push(element.innerElements[element.currentSlide - 1])
+  slides.push(element.innerElements[element.currentSlide])
+  slides.push(element.innerElements[element.currentSlide + 1])
+
+  slides.forEach(slide => {
+    if (!slide) return
+
+    const sources = slide.querySelectorAll("source, img")
+
+    sources.forEach(source => {
+      if (source.dataset.src) {
+        source.src = source.dataset.src
+      } else if (source.dataset.srcset) {
+        source.srcset = source.dataset.srcset
+      }
+    })
+  })
 }
